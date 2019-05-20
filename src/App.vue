68885c439ae63bad09c227d1557b654728e537ed
Нестерюк Денис Сергеевич <n_ds@mail.ru>
2019-05-21 00:05:57 +0300

    блок диалогов

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
                     <span>Моя страница</span>
                   </a>
                 </li>
-                <li class="active-item">
+                <li>
                   <a href="">
                     <span>Мои друзья</span>
                     <span class="cbadge cbadge_xs">
@@ -27,7 +27,7 @@
                     </span>
                   </a>
                 </li>
-                <li>
+                <li class="active-item">
                   <a href="">
                     <span>Мои сообщения</span>
                     <span class="cbadge cbadge_xs">
@@ -44,7 +44,59 @@
             </nav>
           </div>
           <div class="content">
-            обычный текст
+            <ul class="dialogs">
+              <li>
+                <div class="dialog-wrap">
+                  <div class="dialog padding-flank clearfix">
+                    <div class="dialog__photo">
+                    </div>
+                    <div class="dialog__body">
+                      <div class="dialog__header">
+                        <span class="talker">Нестерюк Юлия</span>
+                        <span class="dialog__close">x</span>
+                      </div>
+                       <div class="last-msg">
+                         Предлагаемый стиль - это форма clearfix с обратной совместимостью. Я нашел...
+                       </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div class="dialog-wrap">
+                  <div class="dialog padding-flank clearfix">
+                    <div class="dialog__photo">
+                    </div>
+                    <div class="dialog__body">
+                      <div class="dialog__header">
+                        <span class="talker">Плотников Василий</span>
+                        <span class="dialog__close">x</span>
+                      </div>
+                       <div class="last-msg">
+                         Это текст-"рыба", часто используемый в 
+                       </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div class="dialog-wrap">
+                  <div class="dialog padding-flank clearfix">
+                    <div class="dialog__photo">
+                    </div>
+                    <div class="dialog__body">
+                      <div class="dialog__header">
+                        <span class="talker">Курганов Леха</span>
+                        <span class="dialog__close">x</span>
+                      </div>
+                       <div class="last-msg">
+                         Это текст-"рыба", часто используемый в 
+                       </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
